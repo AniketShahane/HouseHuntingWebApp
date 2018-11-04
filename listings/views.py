8a14f8cb7349a1e10de11cb78c611492eee1c385
Aniket Shahane <shahaneaniket11@gmail.com>
2018-11-04 17:52:16 +0530

    Add Search functionality

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Listing
+from listings.choices import states_choices, price_choices, bedroom_choices
 # Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -18,4 +19,39 @@ def listing(request, listing_id):
     })
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryList = Listing.objects.order_by('-list_date')
+
+    # Filtering has been done below
+    #001 Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryList = queryList.filter(description__icontains=keywords)
+    #002 City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryList = queryList.filter(city__iexact=city)
+    #003 State
+    if 'state' in request.GET:
+        state = request.GET['state']
+        if state:
+            queryList = queryList.filter(state__iexact=state)
+    #004 Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryList = queryList.filter(bedrooms__lte=bedrooms)
+    #005 Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryList = queryList.filter(price__lte=price)
+
+    return render(request, 'listings/search.html',{
+    'states_choices':states_choices,
+    'bedroom_choices':bedroom_choices,
+    'price_choices':price_choices,
+    'listings': queryList,
+    'values': request.GET
+    })
--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import price_choices, bedroom_choices, states_choices
 # Create your views here.
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     return render(request, 'pages/index.html', {
-    'listings':listings
+    'listings':listings,
+    'states_choices':states_choices,
+    'bedroom_choices':bedroom_choices,
+    'price_choices':price_choices,
     })
 
 def about(request):
